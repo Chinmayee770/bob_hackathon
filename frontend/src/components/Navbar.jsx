b1c3633759014af994df41357fbb126192169cbe
crunchydosa123 <prathamgadkari@gmail.com>
2024-06-15 14:32:34 +0530

    say my name
    heisenberg

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className='border-orange-600 w-full bg-orange-600 flex justify-between mb-8'>
+        <img src='./logo.png' className='w-1/6 h-20 ml-2'></img>
+        <div>Bank of Baroda</div>
+        <div>Bank of Baroda</div>
+        <div>Bank of Baroda</div>
+    </div>
+    <hr className='border-orange-600 border-1 rounded-md ml-8 mr-8'></hr>
+    </>
+  )
+}
+
+export default Navbar
